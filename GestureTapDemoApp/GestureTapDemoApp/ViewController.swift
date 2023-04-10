ef4ba918be9c98a0fbf1d7ea4a12a5aafe8379e5
Md Yusuf Khan <md.yusuf.khan.2660@gmail.com>
2023-04-10 11:21:53 -0400

    base app

---
+++ GestureTapDemoApp/GestureTapDemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GestureTapDemoApp
+//
+//  Created by Md Yusuf Khan on 2023-04-10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
